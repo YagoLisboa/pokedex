f6d2a0b7423b75d77aa36b6c5b5b785d0d5417fe
YagoLisboa <yago.lisboa@live.com>
2024-02-18 01:24:35 -0300

    Criando a primeira requisição com JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log("Sucesso!")
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+// Método de consumo de APIs
+fetch(url).then(function(response) {
+    console.log(response)
+});
+
+console.log(10+10);
