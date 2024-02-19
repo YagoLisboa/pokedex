1597d01985bc483bf21109a4f043f35269ea7ea7
YagoLisboa <yago.lisboa@live.com>
2024-02-19 15:03:34 -0300

    Manipulando o resultado de uma requisição através de uma Promise

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,22 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-// Método de consumo de APIs
-fetch(url).then(function(response) {
-    console.log(response)
-});
+// Método de consumo de APIs com Arrow function
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
+
+// Método convencional
+// fetch(url)
+//     .then(function(response) {
+//         console.log(response)
+//     })
+//     .catch(function(error) {
+//         console.error(error)
+//     })
+//     .finally(function() {
+//         console.log("Requisição concluída!")
+//     });
 
 console.log(10+10);
