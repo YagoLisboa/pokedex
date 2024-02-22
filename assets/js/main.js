7d9e51c3bd5f9aa58d9f92319c503755920296c2
YagoLisboa <yago.lisboa@live.com>
2024-02-22 18:55:29 -0300

    Criando mecanismo de paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,36 @@
-
-function convertPokemonToLi(pokemon){
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#0${pokemon.order}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.image}"
-                    alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
-
 const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById("loadMoreButton")
+const limit = 12;
+let offset = 0;
+
 
 // Método de consumo de APIs com Arrow function e métodos de manipulação de lista
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToLi).join('')
-    pokemonList.innerHTML = newHtml
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon) => `
+         <li class="pokemon ${pokemon.type}">
+             <span class="number">#0${pokemon.order}</span>
+             <span class="name">${pokemon.name}</span>
+             <div class="detail">
+                 <ol class="types">
+                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                 </ol>
+                 <img src="${pokemon.image}"
+                     alt="${pokemon.name}">
+             </div>
+         </li>
+    `).join('')
+
+    pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
-
-
 
 
 // Método de consumo de APIs com Arrow function e iteração convencional
@@ -52,4 +57,20 @@ pokeApi.getPokemons().then((pokemons = []) => {
 //         console.log("Requisição concluída!")
 //     });
 
-console.log(10+10);
+
+// Função descontinuada pela simplificação do código:
+// function convertPokemonToLi(pokemon){
+//      return `
+//          <li class="pokemon ${pokemon.type}">
+//              <span class="number">#0${pokemon.order}</span>
+//              <span class="name">${pokemon.name}</span>
+//              <div class="detail">
+//                  <ol class="types">
+//                      ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+//                  </ol>
+//                  <img src="${pokemon.image}"
+//                      alt="${pokemon.name}">
+//              </div>
+//          </li>
+//      `
+// }
