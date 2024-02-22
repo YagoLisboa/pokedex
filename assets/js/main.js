0446230be41bdbb30d25d90245a49d03b9c1d47f
YagoLisboa <yago.lisboa@live.com>
2024-02-22 03:16:06 -0300

    Manipulando múltiplas requisições em paralelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,19 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon){
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">#0${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.sprites.other.dream_world.front_default}"
                     alt="${pokemon.name}">
             </div>
         </li>
@@ -21,7 +24,8 @@ const pokemonList = document.getElementById('pokemonList')
 
 // Método de consumo de APIs com Arrow function e métodos de manipulação de lista
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
 })
 
 
